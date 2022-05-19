2b536b90d4d4b024d7bcee38399173a91247df5e
marcinzygan <soul@o2.pl>
2022-05-20 00:06:20 +0100

    added search function to search for books , added currentUrl state and changed the api url

--- src/App.js
+++ src/App.js
@@ -2,28 +2,56 @@ import React from "react"
 
 
 export default function App (){
+    //Set state for currentUrl 
+    const [currentUrl , setCurrentUrl] = React.useState("?page=1")
     //Set state for current page 
-    const [currentPage , setCurrenPage] = React.useState(6578)
+    const [currentPage , setCurrenPage] = React.useState(1)
     //Set state for the data 
   const [booksData , setBooksData] = React.useState([])
     // Set state for the Loading msg 
   const [isDataLoading , setIsDataLoading] = React.useState(true)
-  // Set state for fetchError
+    // Set state for fetchError
   const [fetchError , setFetchError] = React.useState(null)
-    // Api  change URL of  the page number then useEffect will rerender if we will add dependancy as a currentPage
-  const API = `https://gnikdroy.pythonanywhere.com/api/book/?page=${currentPage}`
+    // Set state for search Input
+  const [searchInput , setSearchInput] = React.useState({search: ""})
+  
 
-// Function to move to next page 
+    // Api  change URL of  the page number then useEffect will rerender if we will add dependancy as a currentPage
+  const API = `https://gnikdroy.pythonanywhere.com/api/book/${currentUrl}`
+
+
+    // Function to move to next page 
 const nextPage = function (){
-    setCurrenPage(prevState => prevState +1)
+    setCurrenPage(prevState => prevState +1)    
+    setCurrentUrl(`?page=${currentPage + 1}`)
 }
-//Function to move to prev page 
+
+    //Function to move to prev page 
 const prevPage = function(){
     if(currentPage<=1) return;
     setCurrenPage(prevState => prevState -1)
-   
+    setCurrentUrl(`?page=${currentPage  -1}`)
 }
 
+//Function to get search value from input
+const inputChange = function(event){
+    const {name , value } = event.target
+    setSearchInput(prevState => {
+        return{
+        ...prevState,
+            [name]: value
+    }
+    })  
+}
+console.log(searchInput);
+
+//useEffect to keep two states in sync and run everytime searchinput or search is changed 
+
+
+//Function to search for the query 
+const getSearch = function(){
+    setCurrentUrl(`?search=${searchInput.search}`)
+}
 // useEffect to fetch API and rerenders if currentPage will change .
 React.useEffect(()=>{
    
@@ -31,7 +59,7 @@ React.useEffect(()=>{
     const fetchBooks = async () =>{
       try{
           
-        const response = await fetch(API);
+        const response = await fetch(API) ;
         //if there is no response throw an error 
         if (!response.ok) throw Error("Did not recive any data");
         // 
@@ -52,20 +80,29 @@ React.useEffect(()=>{
     fetchBooks();
   },[currentPage ,API])
 
-console.log(currentPage);
 
+
+console.log(searchInput);
 
 return (
     <div>
       
     <h1>hello</h1>
+    <input 
+    type="text" 
+    value={searchInput.search}
+    onChange={inputChange}
+    name="search"
+    >
+    </input>
+    <button onClick={getSearch}>SEARCH</button>
     {  isDataLoading &&
     <p> Loading Data ..... </p> }
     {fetchError && <p>{fetchError}</p>}
     {booksData.map(data => 
     <div key={data.id}>
     <p >{data.title}</p> 
-    <a href={`https://www.gutenberg.org/files/${data.id}/${data.id}-0.txt`}>
+    <a href={`https://www.gutenberg.org/files/${data.id}/${data.id}-h/${data.id}-h.htm`}>
     <img src={`https://www.gutenberg.org/cache/epub/${data.id}/pg${data.id}.cover.medium.jpg`} alt={data.title}></img>
     </a>
     </div>
